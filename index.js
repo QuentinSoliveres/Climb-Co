3c3cc91aa95d58c873220d07a1a35f042c5764a8
Akiya <akiyaraguideau@gmail.com>
2018-09-09 20:52:15 +0200

    put everything in one file index.js

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-Vue.component('climbing-item', {
-  props: ['item'],
-  template: '<li>{{ item.text }}</li>'
-})
 
-var app7 = new Vue({
-  el: '#app-7',
+const app = new Vue({
+  el: '#app',
   data: {
-    shoesList: [
-      { id: 0, text: 'La sportiva' },
-      { id: 1, text: 'Scarpa' },
-      { id: 2, text: 'Black diamond'} ,
-      { id: 3, text: 'Evolv' }
-    ],
-    ropeList: [
-      { id: 0, text: 'La sportiva rope' },
-      { id: 1, text: 'Scarpa rope' },
-      { id: 2, text: 'Black diamond rope'} ,
-      { id: 3, text: 'Evolv rope' }
-    ]
+    currentPage: ''
   }
 })
