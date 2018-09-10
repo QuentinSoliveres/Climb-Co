21446fcdd49e1792324cc496cd046215d72fc412
Raguideau <akiyaraguideau@gmail.com>
2018-09-11 01:20:01 +0200

    shop presque fini mais gros probleme de structure qui rends le second nav inutilisable.
    	il faut soit restructurer la page et changer le css soit trouver une solution "bricolage" ...

--- index.js
+++ index.js
@@ -3,18 +3,48 @@ var app = new Vue({
   el: '#app',
   data: {
     currentPage: 'home',
+    shopList:[],
     shoesList: [
-      { id: 0, title: 'Katana', brand: 'La sportiva',  price: 500, image: '' },
-      { id: 1, title: 'Scarpa' },
-      { id: 2, title: 'Black diamond'} ,
-      { id: 3, title: 'Evolv' }
+      { id: 0, title: 'Katana', brand: 'La Sportiva',  price: 135, image: 'images/katana.jpg', description:"put some desc here!! just to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to filljust to fill"},
+      { id: 1, title: 'Boostic', brand: 'Scarpa',  price: 115, image: 'images/Boostic.jpg',  description:"put some desc here!!"},
+      { id: 2, title: 'Bandit', brand: 'Evolv', price: 94, image: 'images/Bandit.jpg', description:"put some desc here!!" } ,
+      { id: 3, title: 'Rock Pillars', brand: 'Diamond',  price: 120, image: 'images/rock-pillars.jpg', description:"put some desc here!!" }
     ],
     ropeList: [
       { id: 0, title: 'La sportiva rope' },
       { id: 1, title: 'Scarpa rope' },
       { id: 2, title: 'Black diamond rope'} ,
       { id: 3, title: 'Evolv rope' }
+    ],
+    harnessesList: [
+      { id: 0, title: 'La sportiva rope' },
+      { id: 1, title: 'Scarpa rope' },
+      { id: 2, title: 'Black diamond rope'} ,
+      { id: 3, title: 'Evolv rope' }
+    ],
+    descendersList: [
+      { id: 0, title: 'La sportiva rope' },
+      { id: 1, title: 'Scarpa rope' },
+      { id: 2, title: 'Black diamond rope'} ,
+      { id: 3, title: 'Evolv rope' }
+    ],
+    carabinersList: [
+      { id: 0, title: 'La sportiva rope' },
+      { id: 1, title: 'Scarpa rope' },
+      { id: 2, title: 'Black diamond rope'} ,
+      { id: 3, title: 'Evolv rope' }
+    ],
+    helmetsList: [
+      { id: 0, title: 'La sportiva rope' },
+      { id: 1, title: 'Scarpa rope' },
+      { id: 2, title: 'Black diamond rope'} ,
+      { id: 3, title: 'Evolv rope' }
     ]
+  },
+  methods: {
+  	setList:function (list){
+  		this.shopList=list
+  	}
   }
 })
 
@@ -25,13 +55,35 @@ var app = new Vue({
   Vue.component('shop', {
 
     props: ['store'],
-    template: ` <section id="shop"> 
+    template: ` <div class="storeContainer">
+    			<nav>
+    				<ul>
+    					<li><a v-on:click.native="setList(shoesList)" >Shoes</a> </li>
+	    				<li><a v-on:click.native="setList(helmetsList)">Helmets</a></li>
+	    				<li><a v-on:click="setList(harnessesList)">Harnesses</a></li>
+	    				<li><a v-on:click="setList(descendersList)">Descenders</a></li>
+	    				<li><a v-on:click="setList(ropeList)">Ropes</a></li>
+	    				<li><a v-on:click="setList(carabinersList)">Carabiners</a></li>
+    				</ul>
+    			</nav>
+
+    			<section id="shop"> 
 				  <h1>Titre de ma page shop</h1>   
-				  <p>Mon super texte</p>
-				  	    magasin:
-				  	     <li v-for="value in store">
-				  	      nom:  {{ value.text }}
-		  	     </li> </section>
+				  	    <h2>magasin:</h2>
+				  	     <div class="itemContainer" v-for="value in store">
+				  	      <img v-bind:src="value.image" width="300" height="300" >
+				  	      <div class="itemInfoColumn1">
+					  	      <h3> {{ value.title }} </h3> 
+					  	      <h4> {{ value.brand }} </h4> 
+					  	      <p class="price"> price: {{ value.price }} </p>
+				  	      </div> 
+				  	      <div class="itemInfoColumn2">
+				  	      	<p class="productDesc" > {{ value.description }} </p>
+						  	<button class="addTocartBtn" type="button">Add To Cart</button>
+						  </div>
+		  	     </div> 
+		  	    </section>
+		  	    </div>
 		`
   })
 
